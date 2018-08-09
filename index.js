5360c38342819396591d02dfbb2bd0a941823842
manon jacquin <manon.jacquin@gmail.com>
2018-08-09 21:30:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var a, b; 
 
 a = Math.floor(Math.random() * 1000);
-
 b = Math.floor(Math.random() * 1000);
 
 function add(a, b){
   return a+b;
 }
 
-a = Math.floor(Math.random() * 1000);
 
+a = Math.floor(Math.random() * 1000);
 b = Math.floor(Math.random() * 1000);
+
+function 
